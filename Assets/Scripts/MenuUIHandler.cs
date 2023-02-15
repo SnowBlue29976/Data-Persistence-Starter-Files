6800995ac5bea239aac59356b6361d4fac92c855
SnowBlue29976 <rodrigo.laborim@gmail.com>
2023-02-15 10:55:21 +0000

    commit to main
    ok

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+public class MenuUIHandler : MonoBehaviour
+{
+    //This is the handler of the main menu scene
+
+    [SerializeField] Text PlayerNameInput;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
